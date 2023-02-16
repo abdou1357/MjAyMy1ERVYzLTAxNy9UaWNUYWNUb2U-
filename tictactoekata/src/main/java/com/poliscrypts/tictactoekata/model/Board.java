ccf0a53bf33ef721f7713cea6a70304f87d12629
Abdou1357 <a_cheridi@esi.dz>
2023-02-16 13:17:57 +0100

    Implement : createNewGame method

--- tictactoekata/src/main/java/com/poliscrypts/tictactoekata/model/Board.java
+++ tictactoekata/src/main/java/com/poliscrypts/tictactoekata/model/Board.java
@@ -18,4 +18,36 @@ public class Board {
     private UUID id;
     private Square nextPlayer = Square.BLANK;
     private boolean endBoard;
+    private Square topLeft = Square.BLANK;
+    private Square topCenter = Square.BLANK;
+    private Square topRight = Square.BLANK;
+    private Square centerLeft = Square.BLANK;
+    private Square center = Square.BLANK;
+    private Square centerRight = Square.BLANK;
+    private Square bottomLeft = Square.BLANK;
+    private Square bottomCenter = Square.BLANK;
+    private Square bottomRight = Square.BLANK;
+
+    public String drawBoard() {
+        String lineSeparatorLabel = "line.separator";
+        String lineSeparatorLine = "+---+---+---+";
+        String placeholder = "| %s | %s | %s |";
+
+        String board;
+        board = System.getProperty(lineSeparatorLabel) +
+                lineSeparatorLine +
+                System.getProperty(lineSeparatorLabel) +
+                String.format(placeholder, topLeft.getValue(), topCenter.getValue(), topRight.getValue()) +
+                System.getProperty(lineSeparatorLabel) +
+                lineSeparatorLine +
+                System.getProperty(lineSeparatorLabel) +
+                String.format(placeholder, centerLeft.getValue(), center.getValue(), centerRight.getValue()) +
+                System.getProperty(lineSeparatorLabel) +
+                lineSeparatorLine +
+                System.getProperty(lineSeparatorLabel) +
+                String.format(placeholder, bottomLeft.getValue(), bottomCenter.getValue(), bottomRight.getValue()) +
+                System.getProperty(lineSeparatorLabel) +
+                lineSeparatorLine;
+        return board;
+    }
 }
--- tictactoekata/src/main/java/com/poliscrypts/tictactoekata/service/impl/TicTacToeServiceImpl.java
+++ tictactoekata/src/main/java/com/poliscrypts/tictactoekata/service/impl/TicTacToeServiceImpl.java
@@ -1,12 +1,48 @@
 package com.poliscrypts.tictactoekata.service.impl;
 
 import com.poliscrypts.tictactoekata.dto.BoardDto;
+import com.poliscrypts.tictactoekata.mapper.BoardMapper;
+import com.poliscrypts.tictactoekata.model.Board;
+import com.poliscrypts.tictactoekata.repository.TicTacToeRepository;
 import com.poliscrypts.tictactoekata.service.TicTacToeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class TicTacToeServiceImpl implements TicTacToeService {
+    private TicTacToeRepository ticTacToeRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(TicTacToeServiceImpl.class);
+    private static final BoardMapper boardMapper = new BoardMapper();
+
+    /**
+     * All possible combinations to win.
+     */
+    private static final List<List<String>> winnerCombinations = Arrays.asList(
+            Arrays.asList("00", "01", "02"),
+            Arrays.asList("10", "11", "12"),
+            Arrays.asList("20", "21", "22"),
+            Arrays.asList("00", "10", "20"),
+            Arrays.asList("01", "11", "21"),
+            Arrays.asList("02", "12", "22"),
+            Arrays.asList("00", "11", "22"),
+            Arrays.asList("02", "11", "20")
+    );
+
+    public TicTacToeServiceImpl(TicTacToeRepository ticTacToeRepository) {
+        this.ticTacToeRepository = ticTacToeRepository;
+    }
 
     @Override
     public BoardDto createNewGame() {
-        return null;
+        var board = ticTacToeRepository.save(new Board());
+
+        logger.info("\nWelcome to 3*3 Tic Tac Toe Game: {}\n", board.drawBoard()!= null ? board.drawBoard(): "");
+
+        return boardMapper.apply(board);
     }
+
 }
+
